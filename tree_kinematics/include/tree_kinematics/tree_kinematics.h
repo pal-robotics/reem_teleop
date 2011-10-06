2ccd27b60e66ba132a9b93a823aee1bafeca3960
student7 <student7@7a0e93e2-327f-11df-9b5e-6d4332766821>
2011-10-06 10:09:08 +0000

    All the licenses to make the code public added

--- tree_kinematics/include/tree_kinematics/tree_kinematics.h
+++ tree_kinematics/include/tree_kinematics/tree_kinematics.h
@@ -2,9 +2,23 @@
  * Filename: reem_kinematics.h
  * Author: Marcus Liebhardt
  * Created: 15.03.2011
- * License:
  * Description:
  */
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 
 // general things
 #include <vector>
