c1006f04de9c93fb9319252889eb1e7d3079d640
Marcus Liebhardt <marcus.liebhardt@yujinrobot.com>
2011-12-05 11:16:41 +0900

    More cleaning and commenting

--- reem_teleop_coordinator/src/reem_teleop_coordinator.cpp
+++ reem_teleop_coordinator/src/reem_teleop_coordinator.cpp
@@ -35,9 +35,10 @@
 /** \author Marcus Liebhardt */
 
 /*
- * \description If new adjusted operator's motion has arrived, this node calculates the desired joint position using
- * KDL's tree inverse kinematics solvers, afterwards the result gets checked for self-collision.
- * If there is no self-collision the desired joint positions get sent to the reem_joint_controller.
+ * \description The teleoperation coordinator retrieves transforms for the specified goal frames and calculates
+ * the desired joint position using to reach them with the specified end points/effectors using
+ * KDL's tree inverse kinematics solvers. Afterwards the new joint positions are checked if the would lead to
+ * self-collision. If there is no self-collision the new joint positions are published to the specified topic.
  */
 
 #include <string>
@@ -62,19 +63,30 @@ static const std::string CC_SERVICE = "/upper_body_environment_server/get_state_
 static const std::string PUB_TOPIC_JOINT_STATES_CMD = "/joint_states_cmd";
 static const std::string SUB_TOPIC_JOINT_STATES = "/joint_states";
 
-bool joint_states_valid = false;
-sensor_msgs::JointState old_joint_state; // pointer to old joint states
+
+bool joint_states_valid = false; // flag to indicate, if there is valid information about the joint states
+sensor_msgs::JointState old_joint_state; // old joint states
 sensor_msgs::JointState::ConstPtr joint_states_ptr; // pointer to the current joint states
 
-
+/*
+ * Callback funtion for retrieving the current joint states;
+ * Note: The used joint states 'old_joints_states' get only updated one time. Later on the joint states of the
+ * last loop are used for the IK service call. Using always the current joint states would introduce an unwanted
+ * feedback.
+ */
 void jointStatesCB(const sensor_msgs::JointState::ConstPtr &joint_states)
-{  
+{
   joint_states_ptr = joint_states;
   if(!joint_states_valid)
     old_joint_state = *joint_states;
   joint_states_valid = true;
 }
 
+
+/*
+ * A generic transform function for retrieving the desired transform for the goal frame
+ * and putting it into a stamped pose message.
+ */
 bool getGoalTransform(tf::TransformListener& tf_listener,
                       std::string& root_frame_name,
                       std::string goal_frame_name,
@@ -102,15 +114,18 @@ bool getGoalTransform(tf::TransformListener& tf_listener,
   pose.pose.orientation.z = transform.getRotation().z();
   pose.pose.orientation.w = transform.getRotation().w();
 
-  return true;  
+  return true;
 }
 
-  
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "reem_teleop_coordinator_node");
   ros::NodeHandle nh, nh_private("~");
 
+  /*
+   * Setting up the coordinator
+   */
   int nr_of_endpts = 0;
   std::vector<std::string> endpts;
   endpts.clear();
@@ -119,11 +134,11 @@ int main(int argc, char** argv)
     for(int i = 0; i < nr_of_endpts; ++i)
     {
       std::string elementname;
-      std::string endpt_name;    
+      std::string endpt_name;
       std::stringstream ss;
       ss << "endpoint_" << i;
       ss >> elementname;
-      if(nh_private.getParam(elementname, endpt_name))  
+      if(nh_private.getParam(elementname, endpt_name))
       {
         endpts.push_back(endpt_name);
         ROS_INFO("Added endpoint '%s'", endpts[i].c_str());
@@ -135,21 +150,21 @@ int main(int argc, char** argv)
       }
     }
   }
-  
+
   geometry_msgs::PoseStamped pose;
   std::map<std::string, geometry_msgs::PoseStamped> poses;
-  std::map<std::string, std::string> rel_endpoints_goals; 
+  std::map<std::string, std::string> rel_endpoints_goals;
   for(unsigned int i = 0; i < endpts.size(); ++i)
   {
     std::string elementname;
-    std::string endpt_goal_name;    
+    std::string endpt_goal_name;
     std::stringstream ss;
     ss << "endpoint_" << i << "_goal";
     ss >> elementname;
-    if(nh_private.getParam(elementname, endpt_goal_name))  
+    if(nh_private.getParam(elementname, endpt_goal_name))
     {
       poses.insert(std::map<std::string, geometry_msgs::PoseStamped>::value_type(endpt_goal_name, pose));
-      rel_endpoints_goals.insert(std::map<std::string, std::string>::value_type(endpt_goal_name, endpts[i])); 
+      rel_endpoints_goals.insert(std::map<std::string, std::string>::value_type(endpt_goal_name, endpts[i]));
       ROS_INFO("Added endpoint goal '%s'", endpt_goal_name.c_str());
       ROS_INFO("Goal '%s' is linked to endpoint '%s'", endpt_goal_name.c_str(), endpts[i].c_str());
     }
@@ -157,51 +172,54 @@ int main(int argc, char** argv)
     {
       ROS_FATAL("Couldn't get the name of an endpoint goal! Aborting...");
       return 0;
-    }  
+    }
   }
-  
+
   std::string root_frame_name;
-  if(nh_private.getParam("root_frame_name", root_frame_name))  
+  if(nh_private.getParam("root_frame_name", root_frame_name))
     ROS_INFO("root frame name: %s", root_frame_name.c_str());
   else
   {
     ROS_FATAL("Couldn't get the name of the root frame! Aborting...");
     return 0;
   }
-  
+
   tf::TransformListener tf_listener;
   tf::TransformBroadcaster tf_broadcaster;
   geometry_msgs::TransformStamped transform;
 
   // subscriber for current joint states
-  ros::Subscriber sub_joint_states = nh.subscribe(SUB_TOPIC_JOINT_STATES, 20, jointStatesCB); 
-  
+  ros::Subscriber sub_joint_states = nh.subscribe(SUB_TOPIC_JOINT_STATES, 20, jointStatesCB);
+
   // service client for IK calculations
   tree_kinematics::get_tree_position_ik tree_ik_srv;
-  //ros::ServiceClient tree_ik_srv_client = nh.serviceClient<tree_kinematics::get_tree_position_ik>
-  //(IK_SERVICE, true);
+  ros::service::waitForService(IK_SERVICE);
+  ros::ServiceClient tree_ik_srv_client = nh.serviceClient<tree_kinematics::get_tree_position_ik>
+  (IK_SERVICE, true);
 
-  // service client for FK calculations
-  kinematics_msgs::GetPositionFK tree_fk_srv;
-  //ros::ServiceClient tree_fk_srv_client = nh.serviceClient<kinematics_msgs::GetPositionFK>
-  //(FK_SERVICE, true);
-  
   // service client for self-collision and joint limits checking
   planning_environment_msgs::GetStateValidity::Request state_val_req;
-  planning_environment_msgs::GetStateValidity::Response state_val_res;  
-  //ros::ServiceClient check_state_validity_client = nh.serviceClient<planning_environment_msgs::GetStateValidity>
-  //(CC_SERVICE, true);
+  planning_environment_msgs::GetStateValidity::Response state_val_res;
+  ros::service::waitForService(CC_SERVICE);
+  ros::ServiceClient check_state_validity_client = nh.serviceClient<planning_environment_msgs::GetStateValidity>
+  (CC_SERVICE, true);
   bool no_self_collision = false;
   bool check_self_collision, check_joint_limits = true;
   nh_private.param("check_self_collision", check_self_collision, true);
   nh_private.param("check_joint_limits", check_joint_limits, true);
   ROS_INFO("checking for self-collision: %s, checking for joint limits: %s", (check_self_collision)?"true":"false",
   (check_joint_limits)?"true":"false");
-  
+
+  // service client for FK calculations
+  kinematics_msgs::GetPositionFK tree_fk_srv;
+  ros::service::waitForService(FK_SERVICE);
+  ros::ServiceClient tree_fk_srv_client = nh.serviceClient<kinematics_msgs::GetPositionFK>
+  (FK_SERVICE, true);
+
   // publisher for joint states commands
   ros::Publisher pub_joint_states_cmd = nh.advertise<sensor_msgs::JointState>(PUB_TOPIC_JOINT_STATES_CMD, 1);
   sensor_msgs::JointState joint_states_cmd;
-  
+
   int loop_rate_value;
   nh_private.param("loop_rate", loop_rate_value, 10);
   ROS_INFO("loop rate: %d", loop_rate_value);
@@ -214,22 +232,14 @@ int main(int argc, char** argv)
   double scc_duration_median = 0.0;
   double cjp_duration = 0.0;
   double cjp_duration_median = 0.0;
-  
-  ros::service::waitForService(IK_SERVICE);
-  ros::ServiceClient tree_ik_srv_client = nh.serviceClient<tree_kinematics::get_tree_position_ik>
-  (IK_SERVICE, true);
-  ros::service::waitForService(FK_SERVICE);
-  ros::ServiceClient tree_fk_srv_client = nh.serviceClient<kinematics_msgs::GetPositionFK>
-  (FK_SERVICE, true);
-  ros::service::waitForService(CC_SERVICE);
-  ros::ServiceClient check_state_validity_client = nh.serviceClient<planning_environment_msgs::GetStateValidity>
-  (CC_SERVICE, true);
-  
+
   while (nh.ok())
   {
     ros::spinOnce();
-    
-    // transform goal frames into pose messages      
+
+    /*
+     * Retrieve current transforms for the configured goal frames and put them into pose messages
+     */
     std::map<std::string, geometry_msgs::PoseStamped>::iterator poses_it;
     geometry_msgs::PoseStamped pose;
     bool goal_transforms_valid = true;
@@ -244,11 +254,18 @@ int main(int argc, char** argv)
         goal_transforms_valid = false;
       }
     }
-    
+
+    /*
+     * The main part
+     * If there are joint states and transforms for all goals are available, the IK service gets called for to get new
+     * joint states (positions), the self-collision-checking service gets called and if the new joint positions
+     * are self-collision free they get published on the specified topic.
+     */
     if (joint_states_valid && goal_transforms_valid)
     {
-      // IK calculations
-
+      /*
+       *  IK calculations
+       */
       // feed pose messages for every endpoint into the request
       tree_ik_srv.request.pos_ik_request.clear();
       kinematics_msgs::PositionIKRequest pos_ik_request;
@@ -268,7 +285,7 @@ int main(int argc, char** argv)
       {
         ROS_ERROR("joint_states_ptr invalid! Aborting loop ...");
         continue;
-      }  
+      }
 
       ROS_DEBUG("Following information has been gathered:");
       for(unsigned int i = 0; i < tree_ik_srv.request.pos_ik_request.size(); ++i)
@@ -281,15 +298,15 @@ int main(int argc, char** argv)
             ROS_DEBUG("joint[%d]: %s with position %f",
                       j,
                       tree_ik_srv.request.pos_ik_request[i].ik_seed_state.joint_state.name[j].c_str(),
-                      tree_ik_srv.request.pos_ik_request[i].ik_seed_state.joint_state.position[j]);   
-        }          
+                      tree_ik_srv.request.pos_ik_request[i].ik_seed_state.joint_state.position[j]);
+        }
         ROS_DEBUG("link[%d]: %s with x = %f, y = %f, z = %f", i,
         tree_ik_srv.request.pos_ik_request[i].ik_link_name.c_str(),
         tree_ik_srv.request.pos_ik_request[i].pose_stamped.pose.position.x,
         tree_ik_srv.request.pos_ik_request[i].pose_stamped.pose.position.y,
         tree_ik_srv.request.pos_ik_request[i].pose_stamped.pose.position.z);
       }
-      
+
       ik_duration = ros::Time::now().toSec();
       if (tree_ik_srv_client.call(tree_ik_srv))
       {
@@ -298,7 +315,7 @@ int main(int argc, char** argv)
         for(unsigned int i = 0; i < tree_ik_srv.response.solution.joint_state.name.size(); ++i)
         {
           ROS_DEBUG_THROTTLE(1.0, "desired position for joint[%d]('%s'): %f", i,
-          tree_ik_srv.response.solution.joint_state.name[i].c_str(), 
+          tree_ik_srv.response.solution.joint_state.name[i].c_str(),
           tree_ik_srv.response.solution.joint_state.position[i]);
         }
       }
@@ -309,12 +326,14 @@ int main(int argc, char** argv)
       }
       ik_duration = ros::Time::now().toSec() - ik_duration;
       ik_duration_median = ((ik_duration_median * (loop_count - 1)) + ik_duration) / loop_count;
-      
-      // self-collision checking
+
+      /*
+       * self-collision checking
+       */
       state_val_req.robot_state = tree_ik_srv.response.solution;
       state_val_req.check_collisions = check_self_collision;
       state_val_req.check_joint_limits = check_joint_limits;
-      
+
       scc_duration = ros::Time::now().toSec();
       if(check_state_validity_client.call(state_val_req, state_val_res))
       {
@@ -328,20 +347,22 @@ int main(int argc, char** argv)
       }
       else
       {
-        ROS_ERROR("Service call to check state validity failed ('%s')! Aborting loop ...", 
+        ROS_ERROR("Service call to check state validity failed ('%s')! Aborting loop ...",
         check_state_validity_client.getService().c_str());
         continue;
       }
       scc_duration = ros::Time::now().toSec() - scc_duration;
       scc_duration_median = ((scc_duration_median * (loop_count - 1)) + scc_duration) / loop_count;
-      
-      // publishing goal joint positions
+
+      /*
+       * Publishing new joint positions, if they are self-collision free
+       */
       if(no_self_collision == true)
       {
         joint_states_cmd = tree_ik_srv.response.solution.joint_state;
         joint_states_cmd.header.stamp = ros::Time::now();
         old_joint_state = joint_states_cmd;
-        no_self_collision = false;       
+        no_self_collision = false;
       }
       else
       {
@@ -351,23 +372,25 @@ int main(int argc, char** argv)
         joint_states_cmd.header.stamp = ros::Time::now();
       }
       pub_joint_states_cmd.publish(joint_states_cmd);
-      
-      // For debug and visualisation purpose: publish forward kinematics messages on tf
+
+      /*
+       * For debug and visualisation purpose: publish forward kinematics messages on tf
+       */
       cjp_duration = ros::Time::now().toSec();
-       
+
       tree_fk_srv.request.header.stamp = ros::Time::now();
-      tree_fk_srv.request.header.frame_id = "/base_footprint";      
+      tree_fk_srv.request.header.frame_id = "/base_footprint";
       tree_fk_srv.request.robot_state.joint_state = tree_ik_srv.response.solution.joint_state;
-      tree_fk_srv.request.robot_state.joint_state = *joint_states_ptr;                  
+      tree_fk_srv.request.robot_state.joint_state = *joint_states_ptr;
       tree_fk_srv.request.fk_link_names.resize(8);
       tree_fk_srv.request.fk_link_names[0] = "arm_right_2_link";
       tree_fk_srv.request.fk_link_names[1] = "arm_right_4_link";
       tree_fk_srv.request.fk_link_names[2] = "hand_right_link";
-      tree_fk_srv.request.fk_link_names[3] = "arm_left_2_link";      
-      tree_fk_srv.request.fk_link_names[4] = "arm_left_4_link";      
+      tree_fk_srv.request.fk_link_names[3] = "arm_left_2_link";
+      tree_fk_srv.request.fk_link_names[4] = "arm_left_4_link";
       tree_fk_srv.request.fk_link_names[5] = "hand_left_link";
       tree_fk_srv.request.fk_link_names[6] = "head_2_link";
-      tree_fk_srv.request.fk_link_names[7] = "torso_2_link";      
+      tree_fk_srv.request.fk_link_names[7] = "torso_2_link";
       if (tree_fk_srv_client.call(tree_fk_srv))
       {
         if(tree_fk_srv.request.fk_link_names.size() == tree_fk_srv.response.pose_stamped.size())
@@ -381,11 +404,11 @@ int main(int argc, char** argv)
             transform.header.seq++;
             transform.header.stamp = ros::Time::now();
             transform.header.frame_id = tree_fk_srv.request.header.frame_id;
-            transform.child_frame_id = name;   
+            transform.child_frame_id = name;
             transform.transform.translation.x = tree_fk_srv.response.pose_stamped[i].pose.position.x;
             transform.transform.translation.y = tree_fk_srv.response.pose_stamped[i].pose.position.y;
-            transform.transform.translation.z = tree_fk_srv.response.pose_stamped[i].pose.position.z;                    
-            transform.transform.rotation = tree_fk_srv.response.pose_stamped[i].pose.orientation;          
+            transform.transform.translation.z = tree_fk_srv.response.pose_stamped[i].pose.position.z;
+            transform.transform.rotation = tree_fk_srv.response.pose_stamped[i].pose.orientation;
             tf_broadcaster.sendTransform(transform);
           }
         }
@@ -395,15 +418,15 @@ int main(int argc, char** argv)
         ROS_ERROR("get_tree_position_fk service call failed! Aborting loop ...");
         continue;
       }
-        
+
       cjp_duration = ros::Time::now().toSec() - cjp_duration;
-      cjp_duration_median = ((cjp_duration_median * (loop_count - 1)) + cjp_duration) / loop_count; 
+      cjp_duration_median = ((cjp_duration_median * (loop_count - 1)) + cjp_duration) / loop_count;
     }
-    
+
     ros::spinOnce();
 
     loop_rate.sleep();
-    
+
     cycle_time_median = ((cycle_time_median * (loop_count - 1)) + loop_rate.cycleTime().toSec()) / loop_count;
 
     ROS_DEBUG_THROTTLE(1.0, "reem_teleop: cycle time %f and median cycle time %f",
@@ -411,14 +434,14 @@ int main(int argc, char** argv)
     ROS_DEBUG_THROTTLE(1.0, "reem_teleop: IKC current duration %f and median %f", ik_duration, ik_duration_median);
     ROS_DEBUG_THROTTLE(1.0, "reem_teleop: SCC current duration %f and median %f", scc_duration, scc_duration_median);
     ROS_DEBUG_THROTTLE(1.0, "reem_teleop: FKC current duration %f and median %f", cjp_duration, cjp_duration_median);
-    
+
     loop_count ++;
   }
   tree_ik_srv_client.shutdown();
-  check_state_validity_client.shutdown();  
+  check_state_validity_client.shutdown();
   pub_joint_states_cmd.shutdown();
   tree_fk_srv_client.shutdown();
-  
+
   return 0;
 }
 
